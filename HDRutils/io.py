a6e7526d7786190588654b726531809c7be7f811
param <catchchaos@github.com>
2021-03-01 10:12:45 +0000

    Restructured logger module

--- HDRutils/io.py
+++ HDRutils/io.py
@@ -1,30 +1,81 @@
+import logging
 import numpy as np
-import imageio as io
+import imageio as io, rawpy
 
-def imread(file):
-	"""
-	Wrapper for io.imread(). Included for completeness
+logger = logging.getLogger(__name__)
 
-	:file: input HDR file (typically in OpenEXR format with extension .exr)
+def imread_libraw(raw, color_space='sRGB', wb='camera'):
+	if color_space == 'sRGB':
+		color_space = rawpy.ColorSpace.sRGB
+	elif color_space == 'raw':
+		color_space = rawpy.ColorSpace.raw
+	elif color_space == 'Adobe':
+		color_space = rawpy.ColorSpace.Adobe
+	else:
+		raise Exception('Unknown color-space. Use sRGB, raw or Adobe.')
+
+	auto_wb = False
+	if wb == 'auto':
+		auto_wb = True
+		wb = raw.daylight_whitebalance
+	elif wb == 'camera':
+		wb = raw.camera_whitebalance
+	elif wb == 'none':
+		wb = np.ones(4)
+	else:
+		raise Exception('Unknown white-balance. Use auto, camera or none.')
+
+	# Different demosaicing algorithms can be used. See this link:
+	# https://letmaik.github.io/rawpy/api/enums.html#demosaicalgorithm
+	# user_flip set to 0 because otherwise we cannot align RAW and demosaiced pixel values
+	img = raw.postprocess(gamma=(1,1), no_auto_bright=True, output_bps=16,
+						  use_auto_wb=auto_wb, user_wb=tuple(wb),
+						  user_flip=0, output_color=color_space,
+						  highlight_mode=rawpy.HighlightMode.Clip)
+	return img
+
+
+def imread(file, libraw=True, color_space='sRGB', wb='camera'):
 	"""
-	return io.imread(file)
+	Wrapper for io.imread() or rawpy.imread() depending on extension provided.
+
+	:file: input file can be a regular image, HDR image (.exr, .hdr) or a
+		   RAW file. Supported RAW formats are (.dng, .arw, .cr2, .nef)
+	:libraw: boolean flag indicating that RAW image should be processed by libraw
+	:color_space: output color_space (libraw argument)
+	:wb: white-balance setting (libraw argument)
+	:return: image as np array
+	"""
+	raw_ext = ('.dng', '.arw', '.cr2', '.nef')
+	if file.lower().endswith((raw_ext)):
+		# Raw formats may be optionally processed with libraw
+		raw = rawpy.imread(file)
+		if libraw:
+			return imread_libraw(raw, color_space, wb)
+		else:
+			return raw.raw_image_visible
+	else:
+		# For other formats, just use imageio
+		return io.imread(file)
+
 
 def imwrite(file, img):
 	"""
 	Wrapper for io.imwrite with some inital sanity checks
 
 	:file: output filename with correct extension
-	:img: HDR image to write(); datatype should be 16-bit integer or floating point (any precision)
+	:img: HDR image to write; datatype should ideally be floating point image
 	"""
-	assert img.dtype != np.uint8, 'It is very unlikely that an HDR image has bit-depth of 8'
-	if img.dtype == np.uint16:
-		assert file.endswith('.png'), 'Use .png for uint16 image'
-		io.imwrite(file, img, format='PNG-FI')
+	if img.dtype in (np.uint8, np.uint16):
+		logger.warning('It is very unlikely that the image is HDR since '
+			f'it is encoded using {img.dtype}')
+		if file.endswith('.png'):
+			io.imwrite(file, img, format='PNG-FI')
+	elif img.dtype in (np.float32, np.float64, np.float128):
+		img = img.astype(np.float32)
+		if file.endswith(('.exr', '.hdr')):
+			# Imegio needs an additional flag to prevent clipping to (2**16 - 1)
+			io.imwrite(file, img, flags=0x0001)
 	else:
-		if img.dtype in (np.float64, np.float128):
-			img = img.astype(np.float32)
-		assert file.endswith('.exr'), 'Use .exr for floating point image'
-		assert img.dtype == np.float32, f'Unrecognized data type: {img.dtype}'
-
-		# Imegio needs an additional flag to prevent clipping to (2**16 - 1)
-		io.imwrite(file, img, flags=0x0001)
+		logger.warning('Non-standard extension/datatype. Reverting to imageio default')
+		io.imwrite(file, img)
--- HDRutils/merge.py
+++ HDRutils/merge.py
@@ -4,16 +4,17 @@ from fractions import Fraction
 import numpy as np
 import rawpy, exifread
 
-# rawpy documentation - https://letmaik.github.io/rawpy/api/
+import HDRutils
 
-def get_metadata(files, log, color_space, wb):
+logger = logging.getLogger(__name__)
+
+def get_metadata(files, color_space, wb):
 	"""
 	Get metadata from EXIF files and rawpy
 	
 	:files: filenames containing the inpt images
-	:log: instance of logging for warnings and verbose information
 	:color_space: Output color-space. Pick 1 of [sRGB, raw, Adobe]
-	:wb: White-balance to use. Pick 1 of [camera, auto, average, none]
+	:wb: White-balance to use. Pick 1 of [camera, auto, none]
 	:return: A dictonary containing all the metadata
 	"""
 
@@ -47,54 +48,23 @@ def get_metadata(files, log, color_space, wb):
 	raw = rawpy.imread(files[0])
 	data['h'], data['w'] = raw.postprocess().shape[:2]
 	data['black_level'] = np.array(raw.black_level_per_channel)
-	data['saturation_point'] = raw.white_level
-	data['bits'] = np.log2(2**(int(data['saturation_point'] - 1)).bit_length())
+	data['saturation_point'] = raw.white_level - 128	# We see artifacts without this offset
+	data['color_space'] = color_space
+	data['wb'] = wb
 
-	assert color_space in ['sRGB', 'raw', 'Adobe'], \
-		'Unreconized color space. For "color_space" pick 1 of: [sRGB, raw, Adobe]'
-	if color_space == 'sRGB':
-		data['colorspace'] = rawpy.ColorSpace.sRGB
-	elif color_space == 'raw':
-		data['colorspace'] = rawpy.ColorSpace.raw
-	elif color_space == 'Adobe':
-		data['colorspace'] = rawpy.ColorSpace.Adobe
-
-	assert wb in ['camera', 'auto', 'average', 'none'], \
-		'Unreconized white-balance. For "color_space" pick 1 of: [camera, auto, average, none]'
-	if wb == 'auto':
-		data['auto_wb'] = True
-		data['whitebalance'] = raw.daylight_whitebalance
-	else:
-		data['auto_wb'] = False
-	if wb == 'camera':
-		data['whitebalance'] = raw.camera_whitebalance
-		for f in files[1:]:
-			assert rawpy.imread(f).camera_whitebalance == data['whitebalance'], \
-				'Images have different white-balance values. Perhaps you want to pass "average"'
-		data['whitebalance'] = np.array(data['whitebalance'])
-	elif wb == 'average':
-		# Use average wb across all images
-		data['whitebalance'] = np.mean([rawpy.imread(f).camera_whitebalance for f in files], axis=0)
-	elif wb == 'none':
-		data['whitebalance'] = np.ones(4) * 1024
-
-	log.info(f"Stack contains {len(files)} images of size: {data['h']}x{data['w']}")
-	log.info(f"Exp: {data['exp']}")
-	log.info(f"Gain: {data['gain']}")
-	log.info(f"aperture: {data['aperture']}")
-	log.info(f"Black-level: {data['black_level']}")
-	log.info(f"Saturation point: {data['saturation_point']}")
-	log.info(f"Estimated bit-depth: {data['bits']}")
-	log.info(f"Color-space: {color_space}")
-	if data['auto_wb']:
-		log.info('White-balance: auto')
-	else:
-		log.info(f"White-balance: {data['whitebalance']}")
+	logger.info(f"Stack contains {len(files)} images of size: {data['h']}x{data['w']}")
+	logger.info(f"Exp: {data['exp']}")
+	logger.info(f"Gain: {data['gain']}")
+	logger.info(f"aperture: {data['aperture']}")
+	logger.info(f"Black-level: {data['black_level']}")
+	logger.info(f"Saturation point: {data['saturation_point']}")
+	logger.info(f"Color-space: {color_space}")
+	logger.info(f"White-balance: {wb}")
 
 	return data
 
 
-def get_unsaturated(raw, bits, img=None):
+def get_unsaturated(raw, saturation_threshold, img=None):
 	"""
 	Estimate a boolean mask to identify unsaturated pixels. The boolean
 	mask returned is either single channel or 3-channel depending on whether
@@ -106,7 +76,6 @@ def get_unsaturated(raw, bits, img=None):
 	:return: boolean unsaturated mask
 	"""
 
-	saturation_threshold = 2**bits - 128
 	unsaturated = np.logical_and.reduce((raw.raw_image_visible[0::2,0::2] < saturation_threshold,
 										raw.raw_image_visible[1::2,0::2] < saturation_threshold,
 										raw.raw_image_visible[0::2,1::2] < saturation_threshold,
@@ -130,28 +99,43 @@ def get_unsaturated(raw, bits, img=None):
 	return unsaturated
 
 
-def merge(files, demosaic_first=True, color_space='sRGB', wb='camera'):
+def merge(files, align=False, demosaic_first=True, color_space='sRGB', wb='camera'):
 	"""
 	Merge multiple SDR images into a single HDR image after demosacing. This
 	is a wrapper function that extracts metadata and calls the appropriate
 	function.
 
 	:files: filenames containing the inpt images
+	:align: align by estimation homography
 	:demosaic_first: order of operations
 	:color_space: Output color-space. Pick 1 of [sRGB, raw, Adobe]
-	:wb: White-balance values. Pick 1 of [camera, auto, average, none]
+	:wb: White-balance values. Pick 1 of [camera, auto, none]
 	:return: Merged FP32 HDR image
 	"""
-	log = logging.getLogger('merge')
-	data = get_metadata(files, log, color_space, wb)
+	data = get_metadata(files, color_space, wb)
 
-	if demosaic_first:
-		HDR = demosaic_merge(files, data, log)
+	if align:
+		# TODO
+		raise NotImplementedError
 	else:
-		HDR = merge_demosaic(files, data, log)
-	return HDR
+		if demosaic_first:
+			HDR = imread_demosaic_merge(files, data)
+		else:
+			HDR = imread_merge_demosaic(files, data)
+		return HDR
 
-def demosaic_merge(files, metadata, log):
+	# import cv2
+	# imgs = [HDRutils.imread(f)/t for f, t in zip(files, data['exp'])]
+	# imgs = [cv2.resize(img, (0,0), fx=1/2, fy=1/2) for img in imgs] 
+	# # imgs = [((img/np.max(imgs))**(1/2.2)).astype(np.float32) for img in imgs]
+	# # return imgs
+	# # imgs = [HDRutils.imread(f) for f in files]
+	# if align:
+	# 	aligned = HDRutils.align(imgs, 1)
+	# 	return aligned
+
+
+def imread_demosaic_merge(files, metadata):
 	"""
 	First postprocess using libraw and then merge RGB images. This function
 	merges in an online way and can handle a large number of inputs with
@@ -160,34 +144,27 @@ def demosaic_merge(files, metadata, log):
 	:files: filenames containing the inpt images
 	:demosaic_first: order of operations
 	:color_space: Output color-space. Pick 1 of [sRGB, raw, Adobe]
-	:wb: White-balance values. Pick 1 of [camera, auto, average, none]
+	:wb: White-balance values. Pick 1 of [camera, auto, none]
 	:return: Merged FP32 HDR image
 	"""
 
-	log.info('Demosaicing before merging.')
+	logger.info('Demosaicing before merging.')
 	# Check for saturation in shortest exposure
 	shortest_exposure = np.argmin(metadata['exp'] * metadata['gain'] * metadata['aperture'])
-	log.info(f'Shortest exposure is {shortest_exposure}')
+	logger.info(f'Shortest exposure is {shortest_exposure}')
 
 	num_saturated = 0
 	num, denom = np.zeros((2, metadata['h'], metadata['w'], 3))
 	for i, f in enumerate(tqdm.tqdm(files)):
 		raw = rawpy.imread(f)
-
-		# Different demosaicing algorithms can be used. See this link:
-		# https://letmaik.github.io/rawpy/api/enums.html#demosaicalgorithm
-		# user_flip set to 0 because otherwise we cannot align RAW and demosaiced pixel values
-		img = raw.postprocess(gamma=(1,1), no_auto_bright=True, output_bps=16,
-			use_auto_wb=metadata['auto_wb'], user_wb=tuple(metadata['whitebalance']),
-			user_flip=0, output_color=metadata['colorspace'],
-			highlight_mode=rawpy.HighlightMode.Clip)
+		img = HDRutils.io.imread_libraw(raw, color_space=metadata['color_space'], wb=metadata['wb'])
 
 		# Ignore saturated pixels in all but shortest exposure
 		if i == shortest_exposure:
 			unsaturated = np.ones_like(img, dtype=bool) 
-			num_sat = np.count_nonzero(np.logical_not(get_unsaturated(raw, metadata['bits'], img=img))) / 3
+			num_sat = np.count_nonzero(np.logical_not(get_unsaturated(raw, metadata['saturation_point'], img=img))) / 3
 		else:
-			unsaturated = get_unsaturated(raw, metadata['bits'], img=img)
+			unsaturated = get_unsaturated(raw, metadata['saturation_point'], img=img)
 		X_times_t = img / metadata['gain'][i] / metadata['aperture'][i]
 		denom[unsaturated] += metadata['exp'][i]
 		num[unsaturated] += X_times_t[unsaturated]
@@ -195,13 +172,13 @@ def demosaic_merge(files, metadata, log):
 	HDR = (num / denom).astype(np.float32)
 
 	if num_sat > 0:
-		log.warning(f"{num_sat/(metadata['h']*metadata['w']):.3f}% of pixels (n={num_sat}) are \
+		logger.warning(f"{num_sat/(metadata['h']*metadata['w']):.3f}% of pixels (n={num_sat}) are \
 			saturated in the shortest exposure. The values for those pixels will be inaccurate.")
 
 	return HDR
 
 
-def merge_demosaic(files, metadata, log, pattern='RGGB'):
+def imread_merge_demosaic(files, metadata, pattern='RGGB'):
 	"""
 	Merge RAW images before demosaicing. This function merges in an online
 	way and can handle a large number of inputs with little memory.
@@ -209,27 +186,35 @@ def merge_demosaic(files, metadata, log, pattern='RGGB'):
 	:files: filenames containing the inpt images
 	:demosaic_first: order of operations
 	:color_space: Output color-space. Pick 1 of [sRGB, raw, Adobe]
-	:wb: White-balance values. Pick 1 of [camera, auto, average, none]
+	:wb: White-balance values. Pick 1 of [camera, auto, none]
 	:return: Merged FP32 HDR image
 	"""
 
 	# Some sanity checks and logs related to colour-space and white-balnce
-	log.info('Merging before demosaicing.')
-	if metadata['colorspace'] != rawpy.ColorSpace.raw:
-		log.warning('Switching to RAW color-space since it is the only one ' \
+	logger.info('Merging before demosaicing.')
+	if metadata['color_space'] != 'raw':
+		logger.warning('Switching to RAW color-space since it is the only one ' \
 			'supported in the current mode.')
-	if metadata['auto_wb']:
-		log.warning('Auto white-balance not supported. Using daylight whitebalance')
-	wb = metadata['whitebalance']
+	raw = rawpy.imread(files[0])
+	if metadata['wb'] == 'auto':
+		logger.warning('Auto white-balance not supported. Using daylight whitebalance')
+		wb = np.array(raw.daylight_whitebalance)
+	elif metadata['wb'] == 'camera':
+		logger.warning('Make sure that white-balance was not set to "auto" while ' \
+			'capturing the stack. Using white-balance of first image.')
+		wb = np.array(raw.camera_whitebalance)
+	else:
+		raise NotImplementedError
+
 	if pattern == 'RGGB':
 		assert wb[1] == wb[3] or wb[3] == 0
 		wb = wb[:3] / wb.max()
 	else:
-		log.warning('Untested pattern. Make sure your white-balance is correct.')
+		raise NotImplementedError
 
 	# Check for saturation in shortest exposure
 	shortest_exposure = np.argmin(metadata['exp'] * metadata['gain'] * metadata['aperture'])
-	log.info(f'Shortest exposure is {shortest_exposure}')
+	logger.info(f'Shortest exposure is {shortest_exposure}')
 
 	num_saturated = 0
 	num, denom = np.zeros((2, metadata['h'], metadata['w']))
@@ -237,15 +222,14 @@ def merge_demosaic(files, metadata, log, pattern='RGGB'):
 		(metadata['h']//2, metadata['w']//2))
 	for i, f in enumerate(tqdm.tqdm(files)):
 		raw = rawpy.imread(f)
-
 		img = raw.raw_image_visible.astype(np.float32)
 
 		# Ignore saturated pixels in all but shortest exposure
 		if i == shortest_exposure:
 			unsaturated = np.ones_like(raw, dtype=bool) 
-			num_sat = np.count_nonzero(np.logical_not(get_unsaturated(raw, metadata['bits'])))
+			num_sat = np.count_nonzero(np.logical_not(get_unsaturated(raw, metadata['saturation_point'])))
 		else:
-			unsaturated = get_unsaturated(raw, metadata['bits'])
+			unsaturated = get_unsaturated(raw, metadata['saturation_point'])
 		
 		# Subtract black level for linearity
 		img -= black_frame
@@ -258,14 +242,14 @@ def merge_demosaic(files, metadata, log, pattern='RGGB'):
 
 	# Libraw does not support 32-bit values. Use colour-demosaicing instead:
 	# https://colour-demosaicing.readthedocs.io/en/latest/manual.html
-	log.info('Running bilinear demosaicing')
+	logger.info('Running bilinear demosaicing')
 	import colour_demosaicing as cd
-	HDR = cd.demosaicing_CFA_Bayer_bilinear(HDR_bayer, pattern='RGGB')
+	HDR = cd.demosaicing_CFA_Bayer_bilinear(HDR_bayer, pattern=pattern)
 
 	# White-balance
 	HDR = (HDR * wb[np.newaxis, np.newaxis, :]).astype(np.float32)
 	if num_sat > 0:
-		log.warning(f"{num_sat/(metadata['h']*metadata['w']):.3f}% of pixels (n={num_sat}) are \
+		logger.warning(f"{num_sat/(metadata['h']*metadata['w']):.3f}% of pixels (n={num_sat}) are \
 			saturated in the shortest exposure. The values for those pixels will be inaccurate.")
 
 	return HDR
